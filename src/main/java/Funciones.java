be480c1a7925c7d46135d575ba7383e1dc47f3a7
Diego <diegocarrasco1013@gmail.com>
2023-03-16 19:29:12 +0100

    Ejercicio 1.b

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -7,10 +7,12 @@ public class Funciones {
         }
     }
     public static double funcRecursiva2 (double valor, double exponente){
-        if (exponente == 0){
+        if (exponente <= 0){
             return 1;
+        } else if (exponente % 2 != 0){
+            return valor * funcRecursiva2(valor, (exponente - 1) / 2) * funcRecursiva2(valor, (exponente - 1) / 2);
         } else {
-            return valor * funcRecursiva2(valor, exponente / 2) * funcRecursiva2(valor, exponente / 2);
+            return funcRecursiva2(valor, exponente / 2) * funcRecursiva2(valor, exponente / 2);
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,17 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduce un valor: ");
+            double valor = sc.nextDouble();
+            System.out.println("Introduce un exponente: ");
+            double exponente = sc.nextDouble();
+            System.out.println("El resultado de la función recursiva 1 es: " + Funciones.funcRecursiva1(valor, exponente));
+            System.out.println("El resultado de la función recursiva 2 es: " + Funciones.funcRecursiva2(valor, exponente));
+        } catch (Exception e) {
+            System.out.println("No has introducido un número");
+        }
+    }
 }
