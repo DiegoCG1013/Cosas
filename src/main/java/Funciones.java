f36413de15fb28393f19b7c2a66b031c6c8db0b3
Diego <diegocarrasco1013@gmail.com>
2023-03-16 19:19:33 +0100

    Ejercicio 1.b

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -6,5 +6,12 @@ public class Funciones {
             return valor * funcRecursiva1(valor, exponente - 1);
         }
     }
+    public static double funcRecursiva2 (double valor, double exponente){
+        if (exponente == 0){
+            return 1;
+        } else {
+            return valor * funcRecursiva2(valor, exponente / 2) * funcRecursiva2(valor, exponente / 2);
+        }
+    }
 }
 
